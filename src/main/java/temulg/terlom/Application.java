bf48272fae8fd874a64b52da7be43861a1ad83ca
Alex Dubov <oakad@yahoo.com>
2018-08-21 02:34:42 +1000

    Basic javafx
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

--- src/main/java/temulg/terlom/Application.java
+++ src/main/java/temulg/terlom/Application.java
@@ -6,8 +6,41 @@
 
 package temulg.terlom;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
 public class Application extends javafx.application.Application {
 	public static void main(String... args) {
-		System.out.println("AaAA!");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Hello World!");
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Hello World!");
+				Screen screen = Screen.getPrimary();
+				double dpi = screen.getDpi();
+				double scaleX = screen.getOutputScaleX();
+				double scaleY = screen.getOutputScaleY();
+
+				System.out.format("DPI: %g, xs %g, ys %g\n", dpi, scaleX, scaleY);
+
+			}
+		});
+
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
 	}
 }
